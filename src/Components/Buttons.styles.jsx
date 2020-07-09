0193268ae7409d402674641abd018d1576b0e86e
Marco Antonio V Montoya <marcovmontoya@gmail.com>
2020-07-08 21:20:12 -0400

    feat: added new styles for buttons disabled

--- src/Components/Buttons.styles.jsx
+++ src/Components/Buttons.styles.jsx
@@ -8,22 +8,59 @@ const Button = styled.button`
   min-width: 100px;
   cursor: pointer;
   font-family: "Roboto Mono for Powerline", monospace;
+  transition: background-color 0.2s linear, color 0.2s linear;
+  
+  &:hover {
+    background-color: ${defaultTheme.primaryColor};
+    color: ${defaultTheme.textColorOnPrimary};
+  }
+  
+  &:focus {
+    outline: 3px solid ${defaultTheme.primaryHoverColor};
+    outline-offset: 2px;
+  }
+  
+  &:active {
+    background-color: ${defaultTheme.primaryActiveColor};
+    border-color: ${defaultTheme.primaryActiveColor};
+    color: ${defaultTheme.textColorOnPrimary};
+  }
 `;
 
 export const PrimaryButton = styled(Button)`
   background-color: ${defaultTheme.primaryColor};
   border: none;
   color: white;
+  
+  &:disabled {
+    background-color: ${defaultTheme.disabled};
+    color: ${defaultTheme.textOnDisabled};
+    cursor: not-allowed;
+  }
 `;
 
 export const SecondaryButton = styled(Button)`
   border: 2px solid ${defaultTheme.primaryColor};
   color: ${defaultTheme.primaryColor};
+  
+  &:disabled {
+    background: none;
+    border-color: ${defaultTheme.disabled};
+    color: ${defaultTheme.textOnDisabled};
+    cursor: not-allowed;
+  }
 `;
 
 export const TertiaryButton = styled(Button)`
   border: 2px solid transparent;
   color: ${defaultTheme.primaryColor};
+  
+  &:disabled {
+    background: none;
+    border-color: ${defaultTheme.disabled};
+    color: ${defaultTheme.textOnDisabled};
+    cursor: not-allowed;
+  }
 `;
 
 export default PrimaryButton;
