b887ab4741ed354a0d018c6b9d418b9d06252ebe
Marco Antonio V Montoya <marcovmontoya@gmail.com>
2020-07-08 22:15:30 -0400

    feat: added new buttons colors

--- src/Components/Buttons.styles.jsx
+++ src/Components/Buttons.styles.jsx
@@ -1,5 +1,78 @@
 import styled from "styled-components";
 import {defaultTheme, typeScale} from "../utils/HomeTheme";
+import {applyStyleModifiers} from 'styled-components-modifiers';
+
+const BUTTON_MODIFIERS = {
+  small: () => `
+    font-size: ${typeScale.helperText};
+    padding: 8px;
+  `,
+  large: () => `
+    font-size: ${typeScale.header5};
+    padding: 16px 24px;
+  `,
+  warning: () => `
+    background: none;
+    color: ${defaultTheme.status.warningColor};
+    &:hover, &:focus {
+      background-color: ${defaultTheme.status.warningColorHover};
+      outline: 3px solid ${defaultTheme.status.warningColorHover};
+      outline-offset: 2px;
+      border: 2px solid transparent;
+    }
+
+    &:active {
+      background-color: ${defaultTheme.status.warningColorActive};
+    }
+  `,
+  primaryButtonWarning: () => `
+    background-color: ${defaultTheme.status.warningColor};
+    color: ${defaultTheme.textColorInverted};
+  `,
+  secondaryButtonWarning: () => `
+    border: 2px solid ${defaultTheme.status.warningColor};
+  `,
+  error: () => `
+    background: none;
+    color: ${defaultTheme.status.errorColor};
+    &:hover, &:focus {
+      background-color: ${defaultTheme.status.errorColorHover};
+      outline: 3px solid ${defaultTheme.status.errorColorHover};
+      outline-offset: 2px;
+      border: 2px solid transparent;
+    }
+    &:active {
+      background-color: ${defaultTheme.status.errorColorActive};
+    }
+  `,
+  primaryButtonError: () => `
+    background-color: ${defaultTheme.status.errorColor};
+    color: ${defaultTheme.textColorInverted};
+  `,
+  secondaryButtonError: () => `
+    border: 2px solid ${defaultTheme.status.warningColor};
+  `,
+  success: () => `
+    background: none;
+    color: ${defaultTheme.status.successColor};
+    &:hover, &:focus {
+      background-color: ${defaultTheme.status.successColorHover};
+      outline: 3px solid ${defaultTheme.status.successColorHover};
+      outline-offset: 2px;
+      border: 2px solid transparent;
+    }
+    &:active {
+      background-color: ${defaultTheme.status.successColorActive};
+    }
+  `,
+  primaryButtonSuccess: () => `
+    background-color: ${defaultTheme.status.successColor};
+    color: ${defaultTheme.textColorInverted};
+  `,
+  secondaryButtonSuccess: () => `
+    border: 2px solid ${defaultTheme.status.warningColor};
+  `
+}
 
 const Button = styled.button`
   padding: 12px 24px;
@@ -36,7 +109,9 @@ export const PrimaryButton = styled(Button)`
     background-color: ${defaultTheme.disabled};
     color: ${defaultTheme.textOnDisabled};
     cursor: not-allowed;
-  }
+  };
+  
+  ${applyStyleModifiers(BUTTON_MODIFIERS)};
 `;
 
 export const SecondaryButton = styled(Button)`
@@ -48,7 +123,9 @@ export const SecondaryButton = styled(Button)`
     border-color: ${defaultTheme.disabled};
     color: ${defaultTheme.textOnDisabled};
     cursor: not-allowed;
-  }
+  };
+  
+  ${applyStyleModifiers(BUTTON_MODIFIERS)};
 `;
 
 export const TertiaryButton = styled(Button)`
@@ -60,7 +137,9 @@ export const TertiaryButton = styled(Button)`
     border-color: ${defaultTheme.disabled};
     color: ${defaultTheme.textOnDisabled};
     cursor: not-allowed;
-  }
+  };
+  
+  ${applyStyleModifiers(BUTTON_MODIFIERS)};
 `;
 
 export default PrimaryButton;
