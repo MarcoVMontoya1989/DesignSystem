f2440e7e953848782b358919389d20afc38620f0
Marco Antonio V Montoya <marcovmontoya@gmail.com>
2020-07-08 22:16:54 -0400

    feat: applying the dark theme for all components
    With the help of ThemeProvider from styled components that will apply for all components the dark theme dynamically.
    
    Note: using hooks.

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import {
   PrimaryButton,
   SecondaryButton,
   TertiaryButton
 } from "./Components/Buttons.styles";
-import {GlobalStyle} from "./utils/HomeTheme";
+import {GlobalStyle, darkTheme, defaultTheme} from "./utils/HomeTheme";
+import {ThemeProvider} from "styled-components";
 
-const App = () => (
-  <>
-    <PrimaryButton>Hello world </PrimaryButton>
-    <SecondaryButton>Farewell world </SecondaryButton>
-    <TertiaryButton>Hey world </TertiaryButton>
-    <GlobalStyle />
-  </>
-);
+const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
+
+  return (
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(true)}>
+        Dark theme
+      </button>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(false)}>
+        Default theme
+      </button>
+      <div
+        style={{
+          background: useDarkTheme
+            ? defaultTheme.primaryColor
+            : darkTheme.primaryColor,
+          width: "100vw",
+          height: "90vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around"
+        }}>
+        <PrimaryButton modifiers={["small"]}>Hello World</PrimaryButton>
+        <SecondaryButton modifiers={["large"]}>Goodbye World</SecondaryButton>
+        <TertiaryButton>Hey</TertiaryButton>
+        <GlobalStyle />
+      </div>
+    </ThemeProvider>
+  );
+};
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
